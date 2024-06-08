4a6742556baca1e6d1eacc0c5e3f766b41a91070
Juan Huanaco <juanhuanacodev@gmail.com>
2024-06-07 22:02:44 -0500

    improved fog design and enhanced robust logic for SprayMarker script

--- Assets/Scripts/Game System/MarkerSpray.cs
+++ Assets/Scripts/Game System/MarkerSpray.cs
@@ -4,27 +4,56 @@ using UnityEngine;
 
 public class MarkerSpray : MonoBehaviour
 {
-    [SerializeField] private GameObject _sprayPrefab;
-    [SerializeField] private float _sprayDistance = 1.5f;
+    [SerializeField] private GameObject _fogPrefab;
+    [SerializeField] private float _fogInstantiationDistance = 0.3f;
+    private float _durationWhenIncreasing = 1f;
+    private float _increaseScaleIn = 1.1f;
+    private float _fogInstantiationInterval = 1f;
+    private List<GameObject> _listOfInstances = new();
+    private Dictionary<Transform, Coroutine> _dictOfActiveCoroutines = new();
 
-    private List<GameObject> listOfInstances = new();
-
-    public void Spray(){
+    public void StartSpraying(){
+        InvokeRepeating(nameof(Spray), 0, _fogInstantiationInterval);
+    }
+    public void StopSpraying(){
+        CancelInvoke();
+    }
+    public void RemoveFog(){
+        StopAllCoroutines();
+        _listOfInstances.ForEach((x)=>Destroy(x));        
+        _listOfInstances.Clear();
+        _dictOfActiveCoroutines.Clear();
+    }
+    private void Spray(){
         RaycastHit hit;
-        if (Physics.Raycast(transform.position, transform.forward, out hit, _sprayDistance))
-            hit.transform.localScale *= 1.1f;
+        if (Physics.Raycast(transform.position, transform.forward, out hit, _fogInstantiationDistance)){
+            if (_dictOfActiveCoroutines.ContainsKey(hit.transform)){
+                StopCoroutine(_dictOfActiveCoroutines[hit.transform]);
+                _dictOfActiveCoroutines.Remove(hit.transform);
+            }
+            var coroutine = StartCoroutine(IncreaseScaleSmoothly(hit.transform));
+            _dictOfActiveCoroutines.Add(hit.transform, coroutine);
+        }
         else
         {
-            Vector3 spawnPosition = transform.position + transform.forward * _sprayDistance;
-            listOfInstances.Add(Instantiate(_sprayPrefab, spawnPosition, transform.rotation));
+            Vector3 spawnPosition = transform.position + transform.forward * _fogInstantiationDistance;
+            _listOfInstances.Add(Instantiate(_fogPrefab, spawnPosition, transform.rotation));
         }
     }
+    private IEnumerator IncreaseScaleSmoothly(Transform targetTransform){
+        Vector3 originalScale = targetTransform.localScale;
+        Vector3 targetScale = originalScale * _increaseScaleIn;
+        float duration = _durationWhenIncreasing;
+        float elapsedTime = 0;
 
-    public void RemoveFog(){
-        Debug.Log("Deleting Fog");
-        for(int i=0;i<listOfInstances.Count;i++)
-            Destroy(listOfInstances[i]);
-        
-        listOfInstances.Clear();
+        while (elapsedTime < duration)
+        {
+            targetTransform.localScale = Vector3.Lerp(originalScale, targetScale, elapsedTime / duration);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        targetTransform.localScale = targetScale;
+        _dictOfActiveCoroutines.Remove(targetTransform);
     }
 }
