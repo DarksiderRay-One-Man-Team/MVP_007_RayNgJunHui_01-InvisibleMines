831f874db20b8f9af0fb7ed9a4fc844ff3f94433
Jeneesh K Jaison <jeneeshjaisonk@gmail.com>
2024-06-08 17:50:54 +0530

    All done

--- Assets/Scripts/Game System/LethalCheck.cs
+++ Assets/Scripts/Game System/LethalCheck.cs
@@ -1,7 +1,7 @@
 using System.Collections;
-using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Events;
 
 [RequireComponent(typeof(Collider))]
 public class LethalCheck : MonoBehaviour
@@ -11,20 +11,37 @@ public class LethalCheck : MonoBehaviour
 
     public enum State
     {
-        
         Normal,
         EMP
-    } 
+    }
+
     private enum Fire
     {
-        
         OffFire,
         OnFire
     }
-    private Fire _fire ;
+
+    private Fire _fire;
     public State currentState;
     public float _EMPTime = 5.0f;
     public TextMeshProUGUI countdownText;
+
+    
+    public int maxAmmo = 3;
+    private int currentAmmo;
+    public TextMeshProUGUI ammoText;  
+
+    
+    public UnityEvent onEMPFired;
+    public UnityEvent onEMPCountdownComplete;
+    public UnityEvent onOutOfAmmo;
+
+    void Start()
+    {
+        currentAmmo = maxAmmo; 
+        UpdateAmmoDisplay();   
+    }
+
     void Update()
     {
         Debug.LogWarning(currentState);
@@ -37,13 +54,12 @@ public class LethalCheck : MonoBehaviour
                 HandleNormalState();
                 break;
             default:
-                
                 break;
         }
     }
+
     public void SwitchState()
     {
-        
         if (currentState == State.EMP)
         {
             currentState = State.Normal;
@@ -52,35 +68,51 @@ public class LethalCheck : MonoBehaviour
         {
             currentState = State.EMP;
         }
-
-        
     }
+
     void HandleEMPState()
     {
         if (_fire == Fire.OffFire)
         {
-            StartCoroutine(_empDelay(_EMPTime));
+            if (currentAmmo > 0)
+            {
+                
+                onEMPFired?.Invoke();
+                StartCoroutine(_empDelay(_EMPTime));
+                currentAmmo--;
+                UpdateAmmoDisplay(); 
+            }
+            else
+            {
+                Debug.LogWarning("Out of ammo!");
+                onOutOfAmmo?.Invoke(); 
+            }
         }
-        else Debug.LogWarning("Can't fire now");
-        
+        else
+        {
+            Debug.LogWarning("Can't fire now");
+        }
     }
+
     void HandleNormalState()
     {
         
     }
+
     void OnTriggerEnter(Collider col)
     {
-        if (col.TryGetComponent(out Mine mine)&& currentState== State.Normal)
+        if (col.TryGetComponent(out Mine mine) && currentState == State.Normal)
         {
             mine.Explode();
             onLethalInvoked?.Invoke();
         }
     }
+
     IEnumerator _empDelay(float delayTime)
     {
         float elapsedTime = 0f;
-
         _fire = Fire.OnFire;
+
         while (elapsedTime < delayTime)
         {
             float remainingTime = delayTime - elapsedTime;
@@ -89,14 +121,22 @@ public class LethalCheck : MonoBehaviour
             yield return null;
         }
 
-        
         countdownText.text = "Time remaining: 0.0 seconds";
         currentState = State.Normal;
         _fire = Fire.OffFire;
 
-
+        
+        onEMPCountdownComplete?.Invoke();
     }
 
+    
 
+    
+    void UpdateAmmoDisplay()
+    {
+        if (ammoText != null)
+        {
+            ammoText.text = $"EMP: {currentAmmo}/{maxAmmo}";
+        }
+    }
 }
-
