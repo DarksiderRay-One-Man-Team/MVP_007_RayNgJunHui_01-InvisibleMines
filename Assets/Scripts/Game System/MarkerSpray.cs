a55ad4a693d17d071067fbed12c2f87793d92716
Juan Huanaco <juanhuanacodev@gmail.com>
2024-06-10 08:44:12 -0500

    initial version finished

--- Assets/Scripts/Game System/MarkerSpray.cs
+++ Assets/Scripts/Game System/MarkerSpray.cs
@@ -1,21 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
+using Oculus.Interaction.HandGrab;
 using UnityEngine;
 
-public class MarkerSpray : MonoBehaviour
+public class MarkerSpray : MonoBehaviour, IHandGrabUseDelegate
 {
     [SerializeField] private GameObject _fogPrefab;
     [SerializeField] private float _fogInstantiationDistance = 0.3f;
+    [SerializeField] private Transform _triggerTransform;
+    [SerializeField] private Transform _particleSprayOffset;
+    private float _fireThresold = 0.8f;
+    [SerializeField] ParticleSystem _particleSystem;
     private float _durationWhenIncreasing = 1f;
     private float _increaseScaleIn = 1.1f;
     private float _fogInstantiationInterval = 1f;
+    private float _triggerStartRotation = -90;
+    private float _triggerEndRotation = -65;
+    private float _dampedUseStrength = 0;
+    [SerializeField] private float _triggerSpeed = 6f;
+    private float _lastUseTime;
+    private AnimationCurve _strengthCurve = AnimationCurve.EaseInOut(0f,0f,1f,1f);
     private List<GameObject> _listOfInstances = new();
     private Dictionary<Transform, Coroutine> _dictOfActiveCoroutines = new();
-
     public void StartSpraying(){
+        _particleSystem.Play();
         InvokeRepeating(nameof(Spray), 0, _fogInstantiationInterval);
     }
     public void StopSpraying(){
+        _particleSystem.Stop();
         CancelInvoke();
     }
     public void RemoveFog(){
@@ -26,7 +38,7 @@ public class MarkerSpray : MonoBehaviour
     }
     private void Spray(){
         RaycastHit hit;
-        if (Physics.Raycast(transform.position, transform.forward, out hit, _fogInstantiationDistance)){
+        if (Physics.Raycast(_particleSprayOffset.position, _particleSprayOffset.forward, out hit, _fogInstantiationDistance)){
             if (_dictOfActiveCoroutines.ContainsKey(hit.transform)){
                 StopCoroutine(_dictOfActiveCoroutines[hit.transform]);
                 _dictOfActiveCoroutines.Remove(hit.transform);
@@ -36,8 +48,8 @@ public class MarkerSpray : MonoBehaviour
         }
         else
         {
-            Vector3 spawnPosition = transform.position + transform.forward * _fogInstantiationDistance;
-            _listOfInstances.Add(Instantiate(_fogPrefab, spawnPosition, transform.rotation));
+            Vector3 spawnPosition = _particleSprayOffset.position + _particleSprayOffset.forward * _fogInstantiationDistance;
+            _listOfInstances.Add(Instantiate(_fogPrefab, spawnPosition, _particleSprayOffset.rotation));
         }
     }
     private IEnumerator IncreaseScaleSmoothly(Transform targetTransform){
@@ -55,5 +67,43 @@ public class MarkerSpray : MonoBehaviour
 
         targetTransform.localScale = targetScale;
         _dictOfActiveCoroutines.Remove(targetTransform);
+    }
+
+    public void BeginUse()
+    {
+        _dampedUseStrength = 0;
+        _lastUseTime = Time.realtimeSinceStartup;
+    }
+
+    public void EndUse()
+    {
+        StopSpraying();
+    }
+
+    public float ComputeUseStrength(float strength)
+    {
+        float delta = Time.realtimeSinceStartup - _lastUseTime;
+        _lastUseTime = Time.realtimeSinceStartup;
+         if (strength > _dampedUseStrength)
+        {
+            _dampedUseStrength = Mathf.Lerp(_dampedUseStrength, strength, _triggerSpeed * delta);
+        }
+        else
+        {
+            _dampedUseStrength = strength;
+        }
+        float progress = _strengthCurve.Evaluate(_dampedUseStrength);
+        UpdateTriggerProgress(progress);
+        return progress;
+    }
+
+    private void UpdateTriggerProgress(float progress){
+        Vector3 angles = _triggerTransform.localEulerAngles;
+        angles.x = Mathf.Lerp(_triggerStartRotation, _triggerEndRotation, progress);
+        _triggerTransform.localEulerAngles = angles;
+
+        if (progress >= _fireThresold)
+            StartSpraying();
+
     }
 }
